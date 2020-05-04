e3f846a209fa3be76f975162cbed49b0c527cf69
Yuriy <xett03@gmail.com>
2020-05-04 14:46:08 -0400

    working code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require('cli-table');
 var colors = require("colors");
 
 var connection = mysql.createConnection({
@@ -51,16 +50,16 @@ function hello() {
 }
 
 
-function options() {
+function option1() {
 
     inquirer
 
         .prompt({
             name: "options",
             type: "list",
-            message: "\n  What you like to do: ".brightYellow,
+            message: "\n  What you would like to do: \n".brightYellow,
             choices: [
-                "Buy",
+                "Buy Item",
                 "Go Back",
                 "Exit"
             ]
@@ -68,7 +67,7 @@ function options() {
 
         .then(function (answer) {
             switch (answer.options) {
-                case "Buy":
+                case "Buy Item":
                     buy();
                     break;
 
@@ -83,6 +82,35 @@ function options() {
         });
 }
 
+
+function option2() {
+
+    inquirer
+
+        .prompt({
+            name: "options",
+            type: "list",
+            message: "\n  What you would like to do: \n".brightYellow,
+            choices: [
+                "Main Menu",
+                "Exit"
+            ]
+        })
+
+        .then(function (answer) {
+            switch (answer.options) {
+                case "Main Menu":
+                    hello();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+
 function autoMotive() {
 
     connection.query("SELECT item_id, product_name, price FROM products WHERE department_name = 'Automotive'", function (err, results) {
@@ -96,18 +124,54 @@ function autoMotive() {
 
             console.log("  iD# - ".brightCyan + colors.brightRed(itemID) + " - " + colors.brightCyan(productName) + " - " + colors.brightRed(productPrice) + " $".brightCyan)
         }
-        options();
+        option1();
     });
 }
 
 
-var buy = function () {
+function homeSport() {
+
+    connection.query("SELECT item_id, product_name, price FROM products WHERE department_name = 'Home Sport'", function (err, results) {
+        if (err) throw err;
+
+        for (var i = 0; i < results.length; i++) {
+
+            var itemID = results[i].item_id;
+            var productName = results[i].product_name;
+            var productPrice = results[i].price;
+
+            console.log("  iD# - ".brightCyan + colors.brightRed(itemID) + " - " + colors.brightCyan(productName) + " - " + colors.brightRed(productPrice) + " $".brightCyan)
+        }
+        option1();
+    });
+}
+
+
+function outDoor() {
+
+    connection.query("SELECT item_id, product_name, price FROM products WHERE department_name = 'Outdoor'", function (err, results) {
+        if (err) throw err;
+
+        for (var i = 0; i < results.length; i++) {
+
+            var itemID = results[i].item_id;
+            var productName = results[i].product_name;
+            var productPrice = results[i].price;
+
+            console.log("  iD# - ".brightCyan + colors.brightRed(itemID) + " - " + colors.brightCyan(productName) + " - " + colors.brightRed(productPrice) + " $".brightCyan)
+        }
+        option1();
+    });
+}
+
+
+function buy() {
 
     inquirer.prompt([{
 
         name: "productID",
         type: "input",
-        message: "Please enter product ID for product you want:".brightYellow,
+        message: "\n  Please enter product iD# for product you want:\n".brightYellow,
         validate: function (value) {
             if (isNaN(value) === false) {
                 return true;
@@ -117,9 +181,9 @@ var buy = function () {
     },
 
     {
-        name: "productUnits",
+        name: "quantity",
         type: "input",
-        message: "How many units do you want?".brightYellow,
+        message: "\n  How many units do you want?\n".brightYellow,
         validate: function (value) {
             if (isNaN(value) === false) {
                 return true;
@@ -128,9 +192,99 @@ var buy = function () {
         }
     }])
 
+
         .then(function (answer) {
 
+            var query = "Select stock_quantity, price, product_name, department_name FROM products WHERE ?";
 
+            connection.query(query, { item_id: answer.productID }, function (err, res) {
+
+                if (err) throw err;
+
+                var productName = res[0].product_name;
+                var departmentName = res[0].department_name;
+                var price = res[0].price;
+                var stockQuantity = res[0].stock_quantity;
+                var selectedProductID = answer.productID;
+                var selectedQuantity = answer.quantity;
+
+
+                if (stockQuantity === 0) {
+                    console.log("  Sorry! \n  This Item: ".brightRed + productName + "\n  Out of Stock!\n".brightRed)
+                    option2();
+                }
+
+                else if (stockQuantity < selectedQuantity) {
+                    console.log("  Sorry! \n  Not Enough: ".brightRed + productName + "\n  Only ".brightRed + stockQuantity + " In Stock!\n".brightRed)
+                    option2();
+                }
+
+                else if (stockQuantity >= selectedQuantity) {
+
+                    confirm(productName, departmentName, price, stockQuantity, selectedProductID, selectedQuantity);
+                }
+            });
         });
 };
 
+
+function confirm(productName, departmentName, price, stockQuantity, selectedProductID, selectedQuantity, totalPrice) {
+
+    var totalPrice = price * selectedQuantity;
+
+    inquirer
+
+        .prompt({
+            name: "confirm",
+            type: "list",
+            message: "\n  Are you sure you want to buy: ".brightYellow + colors.brightCyan(productName) + "\n  In Quantity: ".brightYellow + colors.brightCyan(selectedQuantity) + "\n  With Total: ".brightYellow + colors.brightRed(totalPrice) + " $\n".brightCyan,
+            choices: [
+                "Yes, Continue",
+                "No"
+            ]
+        })
+
+        .then(function (answer) {
+            switch (answer.confirm) {
+                case "Yes, Continue":
+                    keepBuying(productName, departmentName, totalPrice, stockQuantity, selectedProductID, selectedQuantity);
+                    break;
+
+                case "No":
+                    option2();
+                    break;
+            }
+        });
+}
+
+
+function keepBuying(productName, departmentName, totalPrice, stockQuantity, selectedProductID, selectedQuantity) {
+
+    var updatedStockQuantity = stockQuantity - selectedQuantity;
+
+    var query = "UPDATE products SET ? WHERE ?";
+
+    connection.query(query, [{
+
+        item_id: selectedProductID,
+        stock_quantity: updatedStockQuantity,
+
+    },
+    {
+        item_id: selectedProductID
+    }],
+        function (err, res) {
+
+            if (err) throw err;
+
+            console.log("\n  --------------------------------------".brightYellow)
+            console.log("\n  Congratulations!".brightYellow)
+            console.log("\n  Your Item: ".brightYellow + colors.brightCyan(productName))
+            console.log("\n  In Quantity: ".brightYellow + colors.brightCyan(selectedQuantity))
+            console.log("\n  Have Been Purchased".brightYellow + " for: ".brightYellow + colors.brightRed(totalPrice) + " $".brightCyan)
+            console.log("\n  --------------------------------------".brightYellow)
+
+            option2();
+
+        });
+};
